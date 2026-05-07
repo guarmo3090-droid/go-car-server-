38574d2e5b4f6d8f3e8835dcc3b68532794b1979
guarmo3090-droid <guarmo3090@gmail.com>
2026-05-07 18:44:45 +0300

    init commit

---
+++ models.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"sync"
+)
+
+type Car struct {
+	ID    string `json:"id"`
+	Brand string `json:"brand"`
+	Model string `json:"model"`
+	Year  int    `json:"year"`
+	Price int    `json:"price"`
+}
+
+func (c *Car) Validate() error {
+	if c.Brand == "" || c.Model == "" { 
+        return errors.New("brand and model cannot be empty") 
+    }
+	if c.Year < 1885 || c.Year > 2026 { 
+        return errors.New("incorrect year") 
+    }
+	if c.Price <= 0 { 
+        return errors.New("price must be greater than zero") 
+    }
+	return nil
+}
+
+var (
+	cars  = make(map[string]Book)
+	nextID = 1
+	mu     sync.Mutex
+)
